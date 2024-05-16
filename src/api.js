0aa905bd471c8a720814319acd08de4d59dcdab2
Kavya Suman <kavyasuman30@gmail.com>
2024-05-16 23:31:26 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8c337b3f00msh70a330647bae61ap10d3adjsn10a5b7d93dd7",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "732b368c2fb7e45e782f820d879bae6d";
